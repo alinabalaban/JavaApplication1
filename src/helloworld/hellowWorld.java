123236bb54c6bd90e9ae5b39478afe8c9bb57625
alinabalaban <alina_irina_b@yahoo.com>
2016-02-27 17:38:28 +0200

    test 1

---
+++ src/helloworld/hellowWorld.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author Alina
+ */
+public class hellowWorld {
+    public static void main (String[] args)
+    {
+        display();
+    }
+    public static void display() {
+    System.out.println("hello world");
+}
+}
